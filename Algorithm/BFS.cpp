cda2702271e6008f3bc8e65a0e2777beb2930db4
Kim Jung Yeon <zoemfhs123@gmail.com>
2020-09-11 06:51:31 +0900

    DFS 구현
    DFS를 이용해 이동가능한 경우의 수, 경로 찾기를 구현

--- Algorithm/BFS.cpp
+++ Algorithm/BFS.cpp
@@ -3,15 +3,13 @@
 using namespace std;
 
 /*
-	BFS)
+	BFS) 최단거리, 최단경로
 
 	1. 인접한 노드부터 방문
 	2. Queue를 이용
-	3. DFS보다 많은 메모리 사용
 	4. DFS와 다르게 최단경로를 보장
 */
 
-// 길찾기용
 template <typename T>
 class Queue
 {
@@ -20,10 +18,19 @@ private:
 	int frontIdx{}, rearIdx{ -1 };
 	int curSize{}, maxSize{ 1 };
 private:
+	void deepCopy(const Queue& rhs)
+	{
+		arr = new T[rhs.maxSize];
+		for (int i = 0; i < rhs.maxSize; ++i) arr[i] = rhs.arr[i];
+		frontIdx = rhs.frontIdx;
+		rearIdx = rhs.rearIdx;
+		curSize = rhs.curSize;
+		maxSize = rhs.maxSize;
+	}
 	void resize()
 	{
 		T* newArr{ new T[maxSize * 2] };
-		
+
 		if (frontIdx - rearIdx > 0)
 		{
 			copy(arr + frontIdx, arr + maxSize, newArr);
@@ -39,58 +46,18 @@ private:
 	}
 public:
 	Queue() { arr = new T[maxSize]; }
-	Queue(const Queue& rhs)
-	{
-		arr = new T[rhs.maxSize];
-		for (int i = 0; i < rhs.maxSize; ++i) arr[i] = rhs.arr[i];
-		frontIdx = rhs.frontIdx;
-		rearIdx = rhs.rearIdx;
-		curSize = rhs.curSize;
-		maxSize = rhs.maxSize;
-	}
+	Queue(const Queue& rhs) { deepCopy(rhs); }
 	~Queue() { if (arr) { delete[] arr; arr = nullptr; } }
+
+	Queue& operator= (const Queue& rhs) { if (arr) delete[] arr; deepCopy(rhs); return *this; }
 
 	void push(T value) { if (curSize == maxSize) resize(); arr[++rearIdx %= maxSize] = value; ++curSize; }
 	void pop() { if (!empty()) { ++frontIdx %= maxSize; --curSize; } }
 	int size() const { return curSize; }
 	int capacity() const { return maxSize; }
 	bool empty() const { return !curSize ? true : false; }
-	T front() const { return empty() ? T{} : arr[frontIdx]; }
-};
-
-// 경로 저장용
-template <typename T>
-class Stack
-{
-private:
-	T* arr{};
-	int topIdx{ -1 };
-	int maxSize{ 1 };
-private:
-	void resize()
-	{
-		T* newArr{ new T[maxSize *= 2] };
-		copy(arr, arr + maxSize / 2, newArr);
-		delete[] arr;
-		arr = newArr;
-	}
-public:
-	Stack() { arr = new T[maxSize]; };
-	Stack(const Stack& rhs)
-	{
-		arr = new T[rhs.maxSize];
-		for (int i = 0; i < rhs.maxSize; ++i) arr[i] = rhs.arr[i];
-		topIdx = rhs.topIdx;
-		maxSize = rhs.maxSize;
-	}
-	~Stack() { if (arr) { delete[] arr; arr = nullptr; } }
-
-	void push(T value) { if (topIdx == maxSize - 1) resize(); arr[++topIdx] = value; }
-	void pop() { if (!empty()) --topIdx; }
-	int size() const { return topIdx + 1; }
-	int capacity() const { return maxSize; }
-	bool empty() const { return topIdx == -1 ? true : false; }
-	T top() const { return empty() ? T{} : arr[topIdx]; }
+	void reverse() { for (int i = 0; i < size() / 2; ++i) swap(arr[i], arr[size() - i - 1]); }
+	T& front() const { static T tmp{}; return empty() ? tmp : arr[frontIdx]; }
 };
 
 struct Pos
@@ -101,15 +68,15 @@ struct Pos
 	friend ostream& operator<<(ostream& os, const Pos& rhs) { cout << "(" << rhs.x << ", " << rhs.y << ")"; return os; }
 };
 
-Stack<Pos> BFS(Pos start, Pos end, int** G, Pos size)
+Queue<Pos> BFS(Pos start, Pos end, int** G, Pos size)
 {
 	// 이동방향을 저장하는 배열 (상, 하, 좌, 우)
 	Pos dir[4]{ {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
 	// 부모노드(지나온 노드)를 저장하는 배열
-	Pos** parent{ new Pos * [size.y] };
+	Pos** parent{ new Pos*[size.y] };
 	for (int i = 0; i < size.y; ++i) parent[i] = new Pos[size.x];
-	// 최단이동 경로를 저장하는 스택
-	Stack<Pos> path{};
+	// 최단이동 경로를 저장하는 큐
+	Queue<Pos> path{};
 	// 현재 위치한 노드
 	Pos curPos{};
 
@@ -127,9 +94,9 @@ Stack<Pos> BFS(Pos start, Pos end, int** G, Pos size)
 
 		// 이동가능한 노드인지 검사 & 처리
 		for (int i = 0; i < 4; ++i)
-			if (curPos.x + dir[i].x >= 0 && curPos.x + dir[i].x < size.x &&			// x가 배열범위를 벗어나는가?
-				curPos.y + dir[i].y >= 0 && curPos.y + dir[i].y < size.y &&			// y가 배열범위를 벗어나는가?
-				G[curPos.y + dir[i].y][curPos.x + dir[i].x] == 1)					// 이동가능한 노드인가?
+			if (curPos.x + dir[i].x >= 0 && curPos.x + dir[i].x < size.x &&		// x가 배열범위를 벗어나는가?
+				curPos.y + dir[i].y >= 0 && curPos.y + dir[i].y < size.y &&		// y가 배열범위를 벗어나는가?
+				G[curPos.y + dir[i].y][curPos.x + dir[i].x] == 1)				// 이동가능한 노드인가?
 			{
 				que.push({ curPos.x + dir[i].x, curPos.y + dir[i].y });
 				G[curPos.y + dir[i].y][curPos.x + dir[i].x] = 0;								// 재방문 못하게
@@ -138,6 +105,7 @@ Stack<Pos> BFS(Pos start, Pos end, int** G, Pos size)
 	}
 
 	for (Pos i = end; i != start; i = parent[i.y][i.x]) path.push(i);			// 역추적
+	path.reverse();
 
 	return path;
 }
@@ -147,15 +115,15 @@ int main()
 	int row{}, column{};
 	cin >> row >> column;
 
-	int** Graph{ new int* [row] };
+	int** Graph{ new int*[row] };
 	for (int i = 0; i < row; ++i) Graph[i] = new int[column];
 
 	for (int i = 0; i < row; ++i)
 		for (int j = 0; j < column; ++j)
 			scanf_s("%1d", &Graph[i][j]);	// cin >> Graph[i][j];
 
-	Stack<Pos> path{ BFS(Pos{0, 0}, Pos{column - 1, row - 1}, Graph, Pos{column, row}) };
+	Queue<Pos> path{ BFS(Pos{0, 0}, Pos{column - 1, row - 1}, Graph, Pos{column, row}) };
 
 	cout << "최단거리: " << path.size() << "\n최단경로: ";
-	for (int i = path.size(); i != 0; --i) { cout << path.top() << " "; path.pop(); }
+	for (int i = path.size(); i != 0; --i) { cout << path.front() << " "; path.pop(); }
 }
--- Algorithm/DFS.cpp
+++ Algorithm/DFS.cpp
@@ -3,15 +3,116 @@
 using namespace std;
 
 /*
-	DFS)
+	DFS) 모든경로
 
 	1. 한 방향부터 방문
 	2. Stack or 재귀함수를 이용
-	3. BFS보다 적은 메모리 사용
 	4. BFS와 다르게 최단경로를 보장안함
 */
 
+template <typename T>
+class Stack
+{
+private:
+	T* arr{};
+	int topIdx{ -1 };
+	int maxSize{ 1 };
+private:
+	void deepCopy(const Stack& rhs)
+	{
+		arr = new T[rhs.maxSize];
+		for (int i = 0; i < rhs.maxSize; ++i) arr[i] = rhs.arr[i];
+		topIdx = rhs.topIdx;
+		maxSize = rhs.maxSize;
+	}
+	void resize()
+	{
+		T* newArr{ new T[maxSize *= 2] };
+		copy(arr, arr + maxSize / 2, newArr);
+		delete[] arr;
+		arr = newArr;
+	}
+public:
+	Stack() { arr = new T[maxSize]; };
+	Stack(const Stack& rhs) { deepCopy(rhs); }
+	~Stack() { if (arr) { delete[] arr; arr = nullptr; } }
+
+	Stack& operator=(const Stack& rhs) { if (arr) delete[] arr; deepCopy(rhs); return *this; }
+
+	void push(T value) { if (topIdx == maxSize - 1) resize(); arr[++topIdx] = value; }
+	void pop() { if (!empty()) --topIdx; }
+	int size() const { return topIdx + 1; }
+	int capacity() const { return maxSize; }
+	bool empty() const { return topIdx == -1 ? true : false; }
+	void reverse() { for (int i = 0; i < size() / 2; ++i) swap(arr[i], arr[size() - i - 1]); }
+	T& top() const { static T tmp{}; return empty() ? tmp : arr[topIdx]; }
+};
+
+struct Pos
+{
+	int x{}, y{};
+	bool operator==(const Pos& rhs) { return x == rhs.x && y == rhs.y; }
+	friend ostream& operator<<(ostream& os, const Pos& rhs) { cout << "(" << rhs.x << ", " << rhs.y << ")"; return os; }
+};
+
+Stack<Stack<Pos>> DFS(Pos start, Pos end, int** G, Pos size)
+{
+	// 이동방향을 저장하는 배열 (상, 하, 좌, 우)
+	Pos dir[4]{ {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
+	// 이동경로를 저장하는 스택
+	static Stack<Stack<Pos>> path{};
+	// 현재 위치한 노드
+	Pos curPos{};
+
+	static Stack<Pos> stk{};
+	stk.push(start);
+	G[start.y][start.x] = 0;
+
+	curPos = stk.top();
+
+	// 현재위치가 도착지점이라면 경로를 path에 push후 리턴
+	if (curPos == end) { path.push(stk); return Stack<Stack<Pos>>{}; };
+
+	// 이동가능한 노드인지 검사 & 처리
+	for (int i = 0; i < 4; ++i)
+	{
+		if (curPos.x + dir[i].x >= 0 && curPos.x + dir[i].x < size.x &&			// x가 배열범위를 벗어나는가?
+			curPos.y + dir[i].y >= 0 && curPos.y + dir[i].y < size.y &&			// y가 배열범위를 벗어나는가?
+			G[curPos.y + dir[i].y][curPos.x + dir[i].x] == 1)					// 이동가능한 노드인가?
+		{
+			DFS(Pos{ curPos.x + dir[i].x, curPos.y + dir[i].y }, end, G, size);	// 다음노드부터 재귀
+			G[curPos.y + dir[i].y][curPos.x + dir[i].x] = 1;					// 빠져나온길 갈 수 있는 길로 Set
+			stk.pop();															// 빠져나온길 pop
+		}
+	}
+
+	return path;
+}
+
 int main()
 {
+	int row{}, column{};
+	cin >> row >> column;
 
+	int** Graph{ new int*[row] };
+	for (int i = 0; i < row; ++i) Graph[i] = new int[column];
+
+	for (int i = 0; i < row; ++i)
+		for (int j = 0; j < column; ++j)
+			scanf_s("%1d", &Graph[i][j]);	// cin >> Graph[i][j];
+
+	Stack<Stack<Pos>> path{ DFS(Pos{0, 0}, Pos{column - 1, row - 1}, Graph, Pos{column, row}) };
+
+	cout << "경우의 수: " << path.size() << "\n이동경로: \n";
+	while (!path.empty())
+	{
+		path.top().reverse();
+		while (!path.top().empty())
+		{
+			cout << path.top().top() << " ";
+			path.top().pop();
+		}
+		path.pop();
+		cout << "\n";
+	}
 }
