487d656ff92d3bbbf057ff9e490063821e1ca390
Kim Jung Yeon <zoemfhs123@gmail.com>
2020-10-04 01:02:45 +0900

    LIS 구현
    가장 길이가 긴 증가하는 수열을 구하는 코드를 작성.
    lower_bound를 이용하여 O(n^2)에서 O(nlogn)으로 성능 개선

--- Algorithm/Fibonacci.cpp
+++ Algorithm/Fibonacci.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 	1. 분할정복으로 구현할 시, 중복된 함수호출이 많아 성능저하 -> O(n^2)
 	2. DP를 통해 점화식을 도출
-	3. 어떤 문제를 해결하고 그 문제를 저장해놓음(Memoization)
+	3. 어떤 작은 문제를 해결하고 그 문제를 저장해놓음(Memoization)
 */
 
-int fibonacci(int n)
+unsigned long long fibonacci(int n)
 {
 	// Memoization을 위한 메모리
-	int* arr{ new int[n] };
+	unsigned long long* DP{ new unsigned long long[n] };
 
 	if (n == 0 || n == 1) return n;
 
-	arr[0] = 0;
-	arr[1] = 1;
+	DP[0] = 0;
+	DP[1] = 1;
 
-	for (int i = 2; i <= n; ++i) arr[i] = arr[i - 1] + arr[i - 2];
+	for (int i = 2; i <= n; ++i) DP[i] = DP[i - 1] + DP[i - 2];
 
-	return arr[n];
+	return DP[n];
 }
 
 
---
+++ Algorithm/LIS.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+Longest Increasing Subsequence) O(nlogn)
+
+※ 2중포문을 통해서도 구현할 수 있지만, 이진탐색을 이용해 더 효율적으로 계산가능
+*/
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	// 수열의 개수
+	int N{};
+	cin >> N;
+
+	// 각 원소의 값
+	int* val{ new int[N] };
+	for (int i = 0; i < N; ++i) cin >> val[i];
+
+	// 최장길이 수열을 저장하는 컨테이너
+	vector<int> DP{ val[0] };
+	for (int i = 1; i < N; ++i)
+	{
+		// 해당 수열값이 컨테이너의 끝값보다 크다면 증가하는 수열이니 적재한다.
+		if (DP.back() < val[i]) DP.emplace_back(val[i]);
+		// 해당 수열값이 끝값보다 작다면 해당수열값을 다음으로 큰 위치에 덮어씌운다.
+		else if (DP.back() > val[i]) *lower_bound(DP.begin(), DP.end(), val[i]) = val[i];
+	}
+
+	// 최종적으로 컨테이너에는 가장 길이가 긴 증가하는 수열들이 적재된다.
+	cout << DP.size();
+}
